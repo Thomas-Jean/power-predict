2f2b6aa5ad1102ade4ddc04c00fbb13036fb10f3
Thomas-Jean <thomas.jean486@gmail.com>
2020-08-19 18:45:34 -0400

    additional api tests

--- backend/test_api.py
+++ backend/test_api.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime
 from fastapi.testclient import TestClient
 from fastapi import FastAPI
 
@@ -23,3 +24,43 @@ def test_generic_response(client):
 	assert response.status_code == 200
 	assert isinstance(response_body, list)
 	assert len(response_body) == 1
+
+def test_single_date_response(client):
+	
+	response = client.get('/api/v1/power?start_date=2020-01-01')
+	response_body = response.json()
+
+
+	assert response.status_code == 200
+	assert isinstance(response_body, list)
+	assert len(response_body) == 1
+	assert datetime.fromisoformat(response_body[0]['date']) == datetime(2020,1,1)
+
+
+def test_date_range_response(client):
+	
+	response = client.get('/api/v1/power?start_date=2020-01-01&end_date=2020-01-31')
+	response_body = response.json()
+
+
+	assert response.status_code == 200
+	assert isinstance(response_body, list)
+	assert len(response_body) == 31
+
+def test_unix_time_response(client):
+	
+	response = client.get('/api/v1/power?start_date=0')
+	response_body = response.json()
+
+
+	assert response.status_code == 200
+	assert isinstance(response_body, list)
+	assert len(response_body) == 1
+
+def test_unprocessable_entity(client):
+	
+	response = client.get('/api/v1/power?start_date=x')
+	response_body = response.json()
+
+
+	assert response.status_code == 422
